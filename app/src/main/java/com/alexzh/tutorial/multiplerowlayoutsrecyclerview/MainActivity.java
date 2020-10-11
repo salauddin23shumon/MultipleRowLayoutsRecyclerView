1b4fca82fbe1d2fd62a7f89c52c4a7f5b09651f8
salauddin23shumon <salauddin.23shumon@gmail.com>
2020-10-11 12:27:34 +0600

    Initial commit

---
+++ app/src/main/java/com/alexzh/tutorial/multiplerowlayoutsrecyclerview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.alexzh.tutorial.multiplerowlayoutsrecyclerview;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.OrientationHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alexzh.tutorial.multiplerowlayoutsrecyclerview.dummy.DummyData;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DifferentRowAdapter adapter = new DifferentRowAdapter(DummyData.getData());
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(adapter);
+    }
+}
